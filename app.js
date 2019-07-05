8c9b2e9207e6da5b01ca91071f7b3c3c212e3ede
masterkn <masterkn48@gmail.com>
2019-07-06 00:21:24 +0530

    d

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ if(process.env.NODE_ENV==='production'){
       res.sendFile(path.resolve(__dirname,'client','build','index.html'));
     })
   }
-  const port = process.env.PORT || 8000;
+  const port = process.env.PORT || 5000;
   
   app.listen(port, () => console.log(`Server running on port ${port}`));
   
