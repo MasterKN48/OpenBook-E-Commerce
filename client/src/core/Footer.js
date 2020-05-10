285b0defc87070679691f329459f314c55771980
masterkn48 <masterkn48@gmail.com>
2020-05-10 16:14:32 +0530

    gtag

--- client/src/core/Footer.js
+++ client/src/core/Footer.js
@@ -1,11 +1,17 @@
-import React from 'react'
-import Book from './Book.png';
+import React from "react";
+import Book from "./Book.png";
 export default function Footer() {
-    return (
-        <footer className="bg-dark text-white mt-5 p-4 container-fluid text-center" id="#footer">
-            <img src={Book} alt="book" style={{height:'40px',width:'40px'}}/> <br/>
-             OpenBook -- Online World to buy Book <br />
-             Copyright &copy; {new Date().getFullYear()}   <i className="india flag"></i>
-        </footer> 
-    )
+  return (
+    <footer
+      className="bg-dark text-white mt-5 p-4 container-fluid text-center"
+      id="#footer"
+    >
+      <img src={Book} alt="book" style={{ height: "40px", width: "40px" }} />{" "}
+      <br />
+      OpenBook -- Online World to buy Book <br />
+      It is dummy project, don't use real payment method
+      <br />
+      Copyright &copy; {new Date().getFullYear()} <i className="india flag"></i>
+    </footer>
+  );
 }
