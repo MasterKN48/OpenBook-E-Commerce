acb9131f817683912eae906c752489242fe41c5f
masterkn48 <masterkn48@gmail.com>
2020-05-10 16:50:21 +0530

    fix

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ require("dotenv").config();
 const morgan = require("morgan");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const helmet = require("helmet");
-const compression = require("compression");
 const cookieParser = require("cookie-parser");
 const expressValidator = require("express-validator");
+const helmet = require("helmet");
+const compression = require("compression");
 // Routes
 const auth = require("./routes/auth");
 const user = require("./routes/user");
@@ -23,6 +23,7 @@ mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
+    useUnifiedTopology: true,
   })
   .then(() => console.log("DB Connected"));
 // middlewares
